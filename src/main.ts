aecc7556e474b7d5b094dbca407014cf81b367a3
jaimedtm <jaimepumas95@gmail.com>
2021-03-25 12:03:32 -0600

    f commit

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import 'zone.js'
+
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
 if (environment.production) {
   enableProdMode();
 }
+
+Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
